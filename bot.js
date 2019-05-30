a168624fa82dc7fa0b0171fc2aa54b7d9bc2f8dd
Glitch (cobalt-dreadnought) <none>
2019-05-30 22:39:15 +0000

    Initial commit - catbot sends random fact when users says fact

---
+++ bot.js
@@ -0,0 +1,37 @@
+// Import Botkit
+const { Botkit } = require('botkit');
+const { WebexAdapter } = require('botbuilder-adapter-webex');
+
+var request = require('request');
+var results;
+var res;
+
+const adapter = new WebexAdapter({
+    access_token: process.env.access_token,
+    public_address: process.env.public_address,
+    secret: process.env.secret
+})
+
+
+const controller = new Botkit({
+    webhook_name: 'CatBot',
+    adapter: adapter,
+});
+
+controller.on('message', async(bot, message) => {
+     if(message.text) {
+       const query = message.text.trim();
+       if(query.includes('help')){
+         await bot.reply(message, 'Hi! I\'m Catbot! I will send you a cat fact whenever you ask.');
+         await bot.reply(message, 'Usage: @Catbot fact me');
+       }    
+       else if(query.includes('fact me')){
+         console.log("hit me");
+         let response = await fetch('https://catfact.ninja/fact');
+         response = await response.json();
+         console.log(response);
+         console.log(response.fact);
+         await bot.reply(message, response.fact);    
+      }  
+    }
+});
